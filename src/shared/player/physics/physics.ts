ee3b25747b5478bfa68cb87b9e4e8440d3ccb1a4
nadia8666 <chocklet.cats@gmail.com>
2025-03-08 23:26:20 -0800

    Gravity

--- src/shared/player/physics/physics.ts
+++ src/shared/player/physics/physics.ts
@@ -11,7 +11,27 @@ export const PhysicsHandler = {
 
     // Gravity
     ApplyGravity: (Player:Player) => {
-
+        Player.Speed
+        //Gravity
+        const weight = Player.Physics.Weight // TODO: water weight
+        let Acceleration = Player.ToLocal(Player.Flags.Gravity).mul(weight)
+        
+        //mplify gravity
+        if (Player.Flags.Grounded /*&& self.spd.X > self.p.run_speed && self.dotp < 0*/) {
+            Acceleration = Acceleration.mul(new Vector3(1, -8, 1))
+        }
+        
+        //Air drag
+        //if self.flag.ball_aura and self.dotp < 0.98 then
+        //    Acceleration = vector.AddX(Acceleration, self.spd.X * -0.0002)
+        //else
+        //    Acceleration = vector.AddX(Acceleration, self.spd.X * self.p.air_resist)
+        //end
+        //Acceleration = vector.AddY(Acceleration, self.spd.Y * self.p.air_resist_y)
+        //Acceleration = vector.AddZ(Acceleration, self.spd.Z * self.p.air_resist_z)
+        
+        //Apply acceleration
+        Player.Speed = Player.Speed.add(Acceleration)
     }
 
     // Movement
