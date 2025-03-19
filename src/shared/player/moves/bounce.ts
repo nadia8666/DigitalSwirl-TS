4245fd1fb78d11636eb868bed5fe1cacc8aa1691
nadia8666 <115858781+nadia8666@users.noreply.github.com>
2025-03-19 15:27:44 -0700

    fix stomp dir

--- src/shared/player/moves/bounce.ts
+++ src/shared/player/moves/bounce.ts
@@ -9,7 +9,7 @@ export function CheckBounce(Player:Player) {
     if (Player.Flags.BallEnabled && Player.Input.Button.Bounce.Pressed) {
         Player.Flags.IsBounce = true
         Player.Animation.Current = "Roll"
-        Player.Speed = Player.Speed.mul(new Vector3(.75, 0, 1)).sub(new Vector3(Player.Flags.Bounces === 0 && 5 || 7))
+        Player.Speed = Player.Speed.mul(new Vector3(.75, 0, 1)).sub(new Vector3(0, Player.Flags.Bounces === 0 && 5 || 7, 0))
 
         return true
     }
